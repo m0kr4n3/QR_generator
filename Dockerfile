b13525b0c6024c1bc2854188c2aca2b846c0bb07
m0kr4n3 <jm_abdelmalek@esi.dz>
2023-04-09 04:13:35 +0100

    Add dockerization

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM python:3.7
+
+RUN apt-get update -y
+
+RUN mkdir /app
+
+WORKDIR /app
+
+COPY app/requirements.txt ./
+
+RUN pip install -U pip
+
+RUN pip install -r requirements.txt
+
+COPY app/ ./
+
+EXPOSE 8080
+
+ENTRYPOINT ["python","main.py"]
--- app/main.py
+++ app/main.py
@@ -11,8 +11,8 @@ scheduler = APScheduler()
 
 
 def remove_qr_codes():
-    for f in os.listdir("app/static/qr_codes"):
-        os.remove(os.path.join("app/static/qr_codes", f))
+    for f in os.listdir("static/qr_codes"):
+        os.remove(os.path.join("static/qr_codes", f))
 
 
 @app.route("/", methods=["GET"])
@@ -38,11 +38,11 @@ def generate_qr():
             QR.add_data(text)
             img = QR.make_image(fill_color="black", back_color="white")
             qr_path = f"static/qr_codes/{uuid.uuid4().hex}.png"
-            img.save(f"app/{qr_path}")
+            img.save(qr_path)
 
             result = "Here's your QR code"
         except Exception as e:
-            print(e)
+            print(e);input()
             result = "something is going wrong"
             qr_path = None
         finally:
@@ -53,7 +53,7 @@ def generate_qr():
 
 if __name__ == "__main__":
     scheduler.add_job(
-        id="Delete qr codes", func=remove_qr_codes, trigger="interval", seconds=5
+        id="Delete qr codes", func=remove_qr_codes, trigger="interval", seconds=20
     )
     scheduler.start()
-    app.run(debug=True)
+    app.run(host="0.0.0.0",port=8080,debug=False)
